74e07f2d8158e4353985e816bbf4fd9556afe9b2
Arthur Oliveira <117392440+ArthurZall@users.noreply.github.com>
2023-06-11 16:28:59 -0300

    Estruturação da rota para receber dados do banco

--- categorias/categoriasController.js
+++ categorias/categoriasController.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const Categoria =require("./Categoria");
 const slugify = require("slugify");
 
+//ROTA PRINCIPAL 
 router.get("/categorias", (req, res)=>{
     res.send("Rota para categorias")
 });
 
+//ROTA PARA FORMULARIO DE CATEGORIA
 router.get("/admin/categorias/new", (req, res)=>{
-    res.render("./admin/adminCategoria")
+    res.render("./admin/novaCategoria")
 });
 
+//ROTA PARA ENVIAR OS DADOS AO BANCO
 router.post("/categorias/save", (req,res)=>{
     let titulo = req.body.titulo;
 
@@ -24,8 +27,15 @@ router.post("/categorias/save", (req,res)=>{
         })
 
     }else{
-        res.redirect("/admin/categorias/new");
+        res.redirect("./admin/categorias/new");
     }
 })
+
+//ROTA PARA EXIBIR AS CATEGORIAS E AÇÕES
+
+router.get("/admin/categorias", (req, res)=>{
+    res.render("./admin/admCategorias");
+})
+
 
 module.exports = router;
